80a256c2832677020ad0298405a2c28e2cf24cb6
Mouhamad.Abdallah.92 <mouhamad.abdallah.92@gmail.com>
2010-12-22 20:20:04 +0000

    Now the world actually has a 3D array with appropriate size and positioning :)

--- PSPPractice/PSPPractice/Main.cpp
+++ PSPPractice/PSPPractice/Main.cpp
@@ -6,23 +6,21 @@
 #include "Cube.h" // Header file for the 'Cube' class
 #include "Controller.h" // Header file for the 'Controller' class
 
-int worldSize;
+int worldSize, blockSize;
 Cube ****world;
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv); // The only method call that as of yet needs the command line arguments to initialise GLUT
 
-	float x = 0,y = 0, rotation = 0, worldR = 0;
-	worldSize = 4; //Position variables of the cube
+	float x = 0,y = 0, z = 0, rotation = 0, worldR = 0; //Position variables of the cube
+	worldSize = 4; //Size of the WorldArray in terms of amount of blocks per Dimension
+	blockSize = 8; //Size of blocks dimension(x,y,z)
 	
 	Display *display = new Display(); //Instantiate the graphics class via dynamic memory allocation
-	Triangle *tri = new Triangle(64,64); //Instantiate the 'Triangle' class via dynamic memory allocation
-	Cube *cube = new Cube(32,32,32); //Instantiate the 'Cube' class via dynamic memory allocation
 	Controller *controller = new Controller(); //Instantiate the controls class via dynamic memory allocation
 
 	display->initialise(); //Initialises all OpenGL requirements and creates a viewport
-	tri->initialise(1.0f,0,1.0f); //Initialises the triangles color property(R,G,B)
 
 	//initialising a multi-dimensional array via dynamic memory allocation using pointers
 	world = new Cube***[1]; //Initialise a dummy dimension which will simply act as a pointer to the 3D array
@@ -39,14 +37,14 @@ int main(int argc, char **argv)
 		}
 	}
 
-	for(int i = 0; i < worldSize; i++) //Initialise and dynamically allocate a 3D PointerArray new cube objects
+	for(int i = 0, x = 0; i < worldSize; i++, x += blockSize) //Initialise and dynamically allocate a 3D PointerArray new cube objects
 	{
-		for(int j = 0; j < worldSize; j++)
+		for(int j = 0, y = 0; j < worldSize; j++, y+= blockSize)
 		{
-			for(int k = 0; k < worldSize; k ++)
+			for(int k = 0, z = 0; k < worldSize; k ++, z+=blockSize)
 			{
-				world[i][j][k] = new Cube(8*(i+1),8*(i+1),8*(i+1)); //Instantiate a new object and assign it via dynamic memory allocation
-				world[i][j][k]->position->set(32*(i+1), 32*(j+1), 32*(k+1)); //Position the cubes apart from each other in order of theyre relative co-ordinates in the array
+				world[i][j][k] = new Cube(blockSize, blockSize, blockSize); //Instantiate a new object and assign it via dynamic memory allocation
+				world[i][j][k]->position->set(x, y, z); //Position the cubes apart from each other in order of theyre relative co-ordinates in the array
 			}
 		}
 	}
@@ -60,26 +58,33 @@ int main(int argc, char **argv)
 
 		display->translate(0,0,-150); //Push the scene inside of the viewport frustum
 
-		cube->position->x = x; //Set 'x' position of the cube
-		cube->position->y = y; //Set 'y' position of the cube
-
-		if(controller->isKeyDown(PSP_CTRL_LEFT)) //Check for left press
+		if(controller->isKeyDown(PSP_CTRL_LEFT)) //Check for left press to move the world
 		{
 			x -= 4;
 		}
-		if(controller->isKeyDown(PSP_CTRL_RIGHT)) //Check for right press
+		if(controller->isKeyDown(PSP_CTRL_RIGHT)) //Check for right press to move the world
 		{
 			x += 4;
 		}
-		if(controller->isKeyDown(PSP_CTRL_DOWN)) //Check for down press
+		if(controller->isKeyDown(PSP_CTRL_DOWN)) //Check for down press to move the world
 		{
 			y -= 4;
 		}
-		if(controller->isKeyDown(PSP_CTRL_UP)) //Check for up press
+		if(controller->isKeyDown(PSP_CTRL_UP)) //Check for up press to move the world
 		{
 			y += 4;
 		}
-		if(controller->isKeyDown(PSP_CTRL_CROSS)) //Check for 'x' press
+
+		if(controller->isKeyDown(PSP_CTRL_LTRIGGER)) //Check for LTrigger press to control the zoom of the world
+		{
+			z -= 2;
+		}
+		if(controller->isKeyDown(PSP_CTRL_RTRIGGER)) //Check for RTrigger press to control the zoom of the world
+		{
+			z += 2;
+		}
+
+		if(controller->isKeyDown(PSP_CTRL_CROSS)) //Check for 'x' press to rotate the individual blocks
 		{
 			rotation += 4;
 		}
@@ -88,10 +93,7 @@ int main(int argc, char **argv)
 			worldR += 4;
 		}
 
-		display->translate(0,0,0); //Move the cube over to the left side
-		cube->draw(rotation, 0, 1, 0); //Draw the cube
-
-		display->translate(-40,-60,-30);//Move the 'world' into better view
+		display->translate(x,y,z);//Move the 'world' around in the view
 		display->rotate(worldR,0,1,0);//rotate the world around to see around it
 		for(int i = 0; i < worldSize; i++)
 		{
@@ -99,13 +101,10 @@ int main(int argc, char **argv)
 			{
 				for(int k = 0; k < worldSize; k ++)
 				{
-					world[i][j][k]->draw();// Draw the 'World' pointers array of cubes
+					world[i][j][k]->draw(rotation, 0, 1, 0);// Draw the 'World' pointers array of cubes with rotation applied
 				}
 			}
 		}
-
-		//display->translate(100,0,0); //Move the triangle over to the right
-		//tri->draw(); //Draw the triangle
 
 		display->update(); //Update the display buffer(i.e. swap the buffer)
 	}
