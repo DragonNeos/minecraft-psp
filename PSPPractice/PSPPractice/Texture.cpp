df07894f1d4f694c529ca714f58fcbe05636bc21
Mouhamad.Abdallah.92 <mouhamad.abdallah.92@gmail.com>
2011-01-13 09:27:43 +0000

    The new and improved Texturing Class has FINALLY been completed :D!!!! Also this texturing class automatically flips the Y data in the image so that it starts from the top left hand corner of the image not to bottom left hand corner. This rocks and yes finally we have transperancy!!!! :D

--- PSPPractice/PSPPractice/Texture.cpp
+++ PSPPractice/PSPPractice/Texture.cpp
@@ -3,28 +3,113 @@
 // Modifications: All modifications are documented via Repository on google-code page
 
 #include "Texture.h"
-#include "CTexture.h"
+#include <png.h>
+#include <malloc.h>
+//#include "graphics.h"
 #include "GLLib.h"
 #include <stdio.h>      // Header file for standard file i/o.
 #include <stdlib.h>     // Header file for malloc/free.
 
 Texture::Texture()
 {
-	texture = new CTexture();
 	id = 0;
 }
 
+static int getNextPower2(int width)
+{
+	int b = width;
+	int n;
+	for (n = 0; b != 0; n++) b >>= 1;
+	b = 1 << n;
+	if (b == 2 * width) b >>= 1;
+	return b;
+}
+
+void user_warning_fn(png_structp png_ptr, png_const_charp warning_msg)
+{
+}
 void Texture::loadTexture(char *filename)
 {
-	printf("File will now load\n");
-	texture->LoadTexture(filename);
-	printf("File has had load Attempt\n");
-	if(texture != NULL)
-	{
-		printf("File has been loaded\n");
-		id = texture->texID;
-		initialiseTexture();
+	sprintf(buffer, filename);
+	ourImage = loadImage(buffer);
+
+	initialiseTexture();
+}
+
+Image* Texture::loadImage(char* filename)
+{
+	png_structp png_ptr;
+	png_infop info_ptr;
+	unsigned int sig_read = 0;
+	png_uint_32 width, height;
+	int bit_depth, color_type, interlace_type, x, y;
+	u32* line;
+	FILE *fp;
+	Image* image = (Image*) malloc(sizeof(Image));
+	if (!image) return NULL;
+
+	if ((fp = fopen(filename, "rb")) == NULL) return NULL;
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		free(image);
+		fclose(fp);
+		return NULL;;
 	}
+	png_set_error_fn(png_ptr, (png_voidp) NULL, (png_error_ptr) NULL, user_warning_fn);
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		free(image);
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+		return NULL;
+	}
+	png_init_io(png_ptr, fp);
+	png_set_sig_bytes(png_ptr, sig_read);
+	png_read_info(png_ptr, info_ptr);
+	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, int_p_NULL, int_p_NULL);
+	if (width > 512 || height > 512) {
+		free(image);
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+		return NULL;
+	}
+	image->imageWidth = width;
+	image->imageHeight = height;
+	image->textureWidth = getNextPower2(width);
+	image->textureHeight = getNextPower2(height);
+	png_set_strip_16(png_ptr);
+	png_set_packing(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_gray_1_2_4_to_8(png_ptr);
+	if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
+	png_set_filler(png_ptr, 0xff, PNG_FILLER_AFTER);
+	image->data = (Color*) memalign(16, image->textureWidth * image->textureHeight * sizeof(Color));
+	if (!image->data) {
+		free(image);
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+		return NULL;
+	}
+	line = (u32*) malloc(width * 4);
+	if (!line) {
+		free(image->data);
+		free(image);
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+		return NULL;
+	}
+	for (y = 0; y < height; y++) {
+		png_read_row(png_ptr, (u8*) line, png_bytep_NULL);
+		for (x = 0; x < width; x++) {
+			u32 color = line[x];
+			image->data[x + y * image->textureWidth] =  color;
+		}
+	}
+	free(line);
+	png_read_end(png_ptr, info_ptr);
+	png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+	fclose(fp);
+	return image;
 }
 
 void Texture::initialiseTexture()
@@ -41,25 +126,8 @@ void Texture::initialiseTexture()
 	  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     //glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // scale linearly when image bigger than texture
     //glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // scale linearly when image smalled than texture
-	
-    // 2d texture, level of detail 0 (normal), 3 components (red, green, blue), x size from image, y size from image, 
-    // border 0 (normal), rgb color data, unsigned byte data, and finally the data itself.
-	switch(texture->textureType)
-	{
-		case BMP: //Not Working
-		printf("Texture Image Rasterising\n");
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture->width, texture->height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture->data);
-			break;
-		case PCX: //Not working
-		printf("Texture Image Rasterising\n");	
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->width, texture->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture->data);
-			break;
-		case TGA: //Working However TGA files are too large
-			printf("Texture Image Rasterising\n");
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture->width, texture->height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture->data);
-			break;
-	}
-	
+
+	  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ourImage->imageWidth, ourImage->imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, ourImage->data);
 }
 
 void Texture::bindTexture()
