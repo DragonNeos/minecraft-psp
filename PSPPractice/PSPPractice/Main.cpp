0d0573ce0f6b61350bd729a64811b03ab63770e0
davidkilford <davidkilford@ac3938bf-ed52-bf47-ea7f-27f4861bafc6>
2011-02-07 11:25:12 +0000

    -Added Joystick controls, now rotates the scene based on where you are facing -Improved EBOOT background, changed background texture and the logo no longer overlaps with the icon.

--- PSPPractice/PSPPractice/Main.cpp
+++ PSPPractice/PSPPractice/Main.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>			// Header file for file IO
 #include <pspctrl.h>		// Header file for the PSP's control library
 #include <string.h>			// Header file for c string methods
+#include <math.h>			// Header file for trig methods
 #include "GLLib.h"			// Header file carrying all OpenGL32 and GLUT headers
 #include "Display.h"		// Header file For the 'Display' class
 #include "Triangle.h"		// Header file for the 'Triangle' class
@@ -24,12 +25,15 @@ int worldSize, blockSize, blockNumber;
 Cube ***world;
 Texture *worldTexture;
 Inventory *inventory;
+#define PI 3.14159265
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv); // The only method call that as of yet needs the command line arguments to initialise GLUT
 
 	float x = 0,y = 0, z = 0, rotation = 0, worldR = 0; //Position variables of the cube
+	float yRotation = 0, xRotation = 0, zRotation = 0;	//Rotation amounts for each axis
+	float xRotationAmount = 1, zRotationAmount = 0;		//Vars to remove axis rotation problems
 	worldSize = 6; //Size of the WorldArray in terms of amount of blocks per Dimension
 	blockSize = 4; //Size of blocks dimension(x,y,z)
 	blockNumber = 0;
@@ -110,6 +114,15 @@ int main(int argc, char **argv)
 
 		display->translate(0,0,-50); //Push the scene inside of the viewport frustum
 
+		//Rotate the world according to the joystick
+		display->translate(x,y,z);//Move the 'world' around in the view
+
+		//Names may be confusing, to generate a change on the 'y' axis you rotation around x, vise versa
+		display->rotate(yRotation, 0, 1, 0);	
+		//Changes the axis of rotation depending on how much the y-axis is rotated so it always rotates up or down from the frustum's point of view
+		//Because cos and sin constantly move between 0 and 1 every 180 degrees you can work out how much the world should be rotating around x and z
+		display->rotate(xRotation, cos(yRotation*PI/180), 0, sin(yRotation*PI/180));
+
 		if(controller->isKeyDown(PSP_CTRL_LEFT)) //Check for left press to move the world
 		{
 			x -= 4;
@@ -125,6 +138,33 @@ int main(int argc, char **argv)
 		if(controller->isKeyDown(PSP_CTRL_UP)) //Check for up press to move the world
 		{
 			y += 4;
+		}
+
+		//Joystick controls
+		if(controller->getJoystickX()>143)
+		{
+			if(yRotation>=360) yRotation-=360;		//Check so that rotation doesn't get too low
+			yRotation+=(controller->getJoystickX()-143)/25;
+			printf("Right Joystick: %d\n", controller->getJoystickX());
+		}
+
+		if(controller->getJoystickX()<111)
+		{
+			if(yRotation<=-360) yRotation+=360;		//Check so that rotation doesn't get too low
+			yRotation+=(controller->getJoystickX()-111)/25;
+			printf("Left Joystick: %d\n", controller->getJoystickX());
+		}
+
+		if(controller->getJoystickY()>143)
+		{
+			xRotation+=(controller->getJoystickY()-143)/25;
+			printf("Down Joystick: %d\n", controller->getJoystickY());
+		}
+
+		if(controller->getJoystickY()<111)
+		{
+			xRotation+=(controller->getJoystickY()-111)/25;
+			printf("Up Joystick: %d\n", controller->getJoystickY());
 		}
 
 		if(controller->isKeyDown(PSP_CTRL_LTRIGGER)) //Check for LTrigger press to control the zoom of the world
@@ -188,7 +228,14 @@ int main(int argc, char **argv)
 			worldR += 4;
 		}
 
-				if(controller->isKeyDown(PSP_CTRL_TRIANGLE)) //Check for triangle to display inventory
+		if(controller->isKeyDown(PSP_CTRL_SQUARE)) //Check for circle press to rotate world
+		{
+			yRotation = 0;
+			xRotation = 0;
+			zRotation = 0;
+		}
+
+		if(controller->isKeyDown(PSP_CTRL_TRIANGLE)) //Check for triangle to display inventory
 		{
 
 			if (trianglePreviouslyPressed==false)
@@ -214,7 +261,7 @@ int main(int argc, char **argv)
 			trianglePreviouslyPressed=false;
 		}
 
-		display->translate(x,y,z);//Move the 'world' around in the view
+		
 		display->rotate(worldR,0,1,0);//rotate the world around to see around it
 		
 		for(int i = 0; i < worldSize; i++)
